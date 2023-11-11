de695f1ecb1f51a5f51630b3467482f6e854af49
Serhii Bondarenko <sergiy.ol.bondarenko@gmail.com>
2023-11-11 21:43:45 +0200

    1111

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 
 import './App.css';
 
-const apiUrl = REACT_APP_GMAIL;
+const apiUrl = process.env.REACT_APP_GMAIL;
 console.log(apiUrl);
 
 function App() {
   return (
     <div >
       <p>11111</p>
-
         {/*{apiUrl? <p>apiUrl</p>:null}*/}
     </div>
   );
